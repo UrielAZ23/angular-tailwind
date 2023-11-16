104afe69e6be3c7af2017b2b84849c966f73ff47
Uriel Alvarez <uriel.az2303@gmail.com>
2023-11-16 10:56:48 -0600

    se agrega btn dinamico

---
+++ src/app/components/btn/btn.component.ts
@@ -0,0 +1,30 @@
+import { Component,OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-btn',
+  templateUrl: './btn.component.html'
+})
+export class BtnComponent {
+ 
+  @Input() typeBtn: 'button' | 'reset' | 'submit' = 'button';
+  @Input() color='primary';
+
+  constructor(){
+
+  }
+
+  ngOnInit():void{
+
+  }
+
+  get colors(){
+    return {
+      'bg-success-700':this.color==='success',
+      'hover:bg-success-800':this.color==='success',
+      'focus:ring-success-300':this.color==='success',
+      'bg-primary-700':this.color==='primary',
+      'hover:bg-primary-800':this.color==='primary',
+      'focus:ring-primary-300':this.color==='primary',
+    }
+  }
+}
